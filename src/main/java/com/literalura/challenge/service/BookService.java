48221a3f5499593e2ad51ecaaf5dd4194af186c9
EdiedRamos <edieisa22@gmail.com>
2024-06-21 17:18:45 -0500

    chore: add manual mapper and add Book getters

--- src/main/java/com/literalura/challenge/service/BookService.java
+++ src/main/java/com/literalura/challenge/service/BookService.java
@@ -2,6 +2,7 @@ package com.literalura.challenge.service;
 
 import com.literalura.challenge.deserialization.BookDeserializer;
 import com.literalura.challenge.dto.BookDTO;
+import com.literalura.challenge.entity.Book;
 
 public class BookService {
 
@@ -15,6 +16,16 @@ public class BookService {
         JSONConverter jsonConverter = new JSONConverter();
         String resultJson = gutendexRequest.searchByTitle(bookTitle);
         return jsonConverter.toObjectWithBuilder(resultJson, BookDTO.class, new BookDeserializer());
+    }
+
+    //    TODO: This could be omitted with Mappers
+    private Book fromBookDTO(BookDTO bookDTO) {
+        Book book = new Book();
+        book.setTitle(bookDTO.getTitle());
+        book.setAuthor(bookDTO.getAuthor());
+        book.setLanguage(bookDTO.getLanguage());
+        book.setDownloads(bookDTO.getDownloads());
+        return book;
     }
 
 }
